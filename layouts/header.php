3f84e64d1c160df3f12d2d43afc27297b463b056
Ashique <ashiquekhan386@gmail.com>
2026-03-23 09:45:27 +0530

    header changes

--- layouts/header.php
+++ layouts/header.php
@@ -99,7 +99,7 @@
             <div class="col-lg-3 d-none d-lg-block">
                 <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100 collapsed" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;" aria-expanded="false">
                     <h6 class="m-0 text-white">Categories</h6>
-                    <i class="fa fa-angle-down text-white"></i>
+                    <i class="fa fa-angle-down text-black"></i>
                 </a>
                 <nav class="position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light collapse" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1111;">
                     <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
