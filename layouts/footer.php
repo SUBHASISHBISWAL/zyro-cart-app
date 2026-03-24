4862b16d2d7e977d0bccb78cb61bfd04da733f26
Ashique <ashiquekhan386@gmail.com>
2026-03-24 19:35:55 +0530

    Full footer update with professional links and contact info

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,5 +1,4 @@
-<!-- Footer Start -->
-    <div class="container-fluid bg-secondary text-dark mt-5 pt-5">
+<div class="container-fluid bg-secondary text-dark mt-5 pt-5">
         <div class="row px-xl-5 pt-5">
             <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
                 <div class="col-md-3">
@@ -8,33 +7,31 @@
                     </h3>
                 </div>
 
-                <p>Dolore erat dolor sit lorem vero amet. Sed sit lorem magna, ipsum no sit erat lorem et magna ipsum dolore amet erat.</p>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
+                <p>ZyroCart is your one-stop destination for premium products at reasonable prices. We are dedicated to providing the best shopping experience with quality and trust.</p>
+                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Bhubaneswar, Odisha, India</p>
+                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>support@zyrocart.com</p>
+                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+91 98765 43210</p>
             </div>
             <div class="col-lg-8 col-md-12">
                 <div class="row">
                     <div class="col-md-4 mb-5">
                         <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
                         <div class="d-flex flex-column justify-content-start">
-                            <a class="text-dark mb-2" href="index.php"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                            <a class="text-dark mb-2" href="pages/shop.php"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-                            <a class="text-dark mb-2" href="pages/detail.php"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-                            <a class="text-dark mb-2" href="pages/cart.php"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-                            <a class="text-dark mb-2" href="pages/checkout.php"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                            <a class="text-dark" href="pages/contact.php"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>index.php"><i class="fa fa-angle-right mr-2"></i>Home</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/shop.php"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/detail.php"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/cart.php"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/checkout.php"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+                            <a class="text-dark" href="<?php echo $base; ?>pages/contact.php"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
                         </div>
                     </div>
                     <div class="col-md-4 mb-5">
-                        <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
+                        <h5 class="font-weight-bold text-dark mb-4">Help & Support</h5>
                         <div class="d-flex flex-column justify-content-start">
-                            <a class="text-dark mb-2" href="index.php"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                            <a class="text-dark mb-2" href="pages/shop.php"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-                            <a class="text-dark mb-2" href="pages/detail.php"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-                            <a class="text-dark mb-2" href="pages/cart.php"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-                            <a class="text-dark mb-2" href="pages/checkout.php"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                            <a class="text-dark" href="pages/contact.php"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/faqs.php"><i class="fa fa-angle-right mr-2"></i>FAQs</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/help.php"><i class="fa fa-angle-right mr-2"></i>Help Center</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/support.php"><i class="fa fa-angle-right mr-2"></i>Support</a>
+                            <a class="text-dark mb-2" href="<?php echo $base; ?>pages/contact.php"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
                         </div>
                     </div>
                     <div class="col-md-4 mb-5">
@@ -58,35 +55,26 @@
         <div class="row border-top border-light mx-xl-5 py-4">
             <div class="col-md-6 px-xl-0">
                 <p class="mb-md-0 text-center text-md-left text-dark">
-                    &copy; <a class="text-dark font-weight-semi-bold" href="#">Your Site Name</a>. All Rights Reserved. Designed
-                    by
-                    <a class="text-dark font-weight-semi-bold" href="https://htmlcodex.com">HTML Codex</a><br>
-                    Distributed By <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                    &copy; <a class="text-dark font-weight-semi-bold" href="#">ZyroCart</a>. All Rights Reserved.<br>
+                    Developed with <i class="fa fa-heart text-primary"></i> by <strong>Ashique, Subashis & Suchismita</strong><br>
+                    <span class="text-muted small">Under the guidance of <strong>Prasenjit Sir</strong></span>
                 </p>
             </div>
             <div class="col-md-6 px-xl-0 text-center text-md-right">
-                <img class="img-fluid" src="<?php echo $base; ?>assets/img/payments.png" alt="">
+                <img class="img-fluid" src="<?php echo $base; ?>assets/img/payments.png" alt="Payments">
             </div>
         </div>
     </div>
-    <!-- Footer End -->
-
-
-    <!-- Back to Top -->
     <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
 
 
-    <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="<?php echo $base; ?>assets/lib/easing/easing.min.js"></script>
     <script src="<?php echo $base; ?>assets/lib/owlcarousel/owl.carousel.min.js"></script>
 
-    <!-- Contact Javascript File -->
     <script src="<?php echo $base; ?>assets/mail/jqBootstrapValidation.min.js"></script>
     <script src="<?php echo $base; ?>assets/mail/contact.js"></script>
 
-    <!-- Template Javascript -->
     <script src="<?php echo $base; ?>assets/js/main.js"></script>
 </body>
-
