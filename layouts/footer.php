762c7508cfa3a2596ffbceb4d6692ff60b259a33
subhasis biswal <subhasishbiswal6372@gmail.com>
2026-03-25 08:41:34 +0530

    footer changes

--- layouts/footer.php
+++ layouts/footer.php
@@ -77,4 +77,6 @@
     <script src="<?php echo $base; ?>assets/mail/contact.js"></script>
 
     <script src="<?php echo $base; ?>assets/js/main.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
